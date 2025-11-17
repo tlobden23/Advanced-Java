db0b7ece0fe68eec50ae33c1e786718214f63ffc
tlobden23 <tlobden79@gmail.com>
2025-11-17 13:16:10 -0500

    Add servlet FreeMarker webapp templates and remove zip
    Added template Jetty web application projects for Class 11 Lab 1, Class 11 Lab 2, and Homework 3, each with FreeMarker integration, example servlets, and supporting files. Removed Homework 5.zip from the repository.

---
+++ Class 11 Lab 2/src/main/java/edu/bhcc/Product.java
@@ -0,0 +1,26 @@
+package edu.bhcc;
+
+public class Product {
+
+    private String name;
+    private double price;
+    private String description;
+
+    public Product(String name, double price, String description){
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
